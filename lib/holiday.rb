242193ca922d7220f180ddb36b69ea813528b5c5
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-29 21:56:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-
+  holiday_hash.each do | season,events|
+    season.each do |events, supplies| return events=="BBQ"
+      
 end
 
 
