67eb915acf5fd618703f27f30b58f7aa0855a5f2
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-29 21:59:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash.each 
+  holiday_hash.each do |season,events| 
+     season.each do |events,supplies|
+       
 end
 
 def all_supplies_in_holidays(holiday_hash)
